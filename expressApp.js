b0a4601c336162e4fd98ef23191f25725e7317ae
thomas <tom.pasqui00@gmail.com>
2022-10-11 11:35:36 +0200

    Es 1.1/1.2

--- expressApp.js
+++ expressApp.js
@@ -1,10 +1,19 @@
 const expressApp = require('express');
 const app = expressApp();
-const movies = require('./movies');
+const { getAllMovies, getHighlightedMovies, getMoviesPage} = require('./movies');
 
 app.get('/', (req, res) => {
-  res.json(movies);
-})
+  res.json(getHighlightedMovies());
+});
+
+app.get('/page/:page', (req, res) => {
+    const page = parseInt(req.params.page);
+    if(page)
+      res.json(getMoviesPage(page));
+    else
+      res.status(400).send('Invalid page number');
+});
+
 
 module.exports = _ => app.listen(3000, _ => {
   console.log(`Express ready at http://localhost:3000/`)
--- movies.js
+++ movies.js
@@ -3,7 +3,10 @@ const { readFileSync } = require('fs');
 let movies;
 if(!movies) movies = JSON.parse(readFileSync('./movies.json').toString());
 
-const getHighlightedMovies = _ => movies.slice(0, 10);
-let getAllMovies = _ => movies;
+const PAGE_SIZE = 20;
 
-module.exports = { getAllMovies, getHighlightedMovies };
+const getHighlightedMovies = _ => movies.slice(0, 10);
+const getMoviesPage = page => movies.slice(page * PAGE_SIZE, (page + 1) * PAGE_SIZE);
+const getAllMovies = _ => movies;
+
+module.exports = { getAllMovies, getHighlightedMovies, getMoviesPage };
